d1cc50dfe380ffb51eaf9d12063dabaa79955d6b
Krunal Kapadiya <krunal3kapadiya@gmail.com>
2023-01-05 00:06:47 +0530

    Adding Content

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
 * Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/Krunal Kapadiya/blob/master/LICENSE)
 */
 //
 // Scripts
